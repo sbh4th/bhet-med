eee3723cfdbc7f618b47239d6480975b11e1d140
sbh4th <sbh4th@gmail.com>
2023-12-21 00:56:02 -0500

    Revise table of simple vs. cohort ATTs

--- code/bhet-med-analysis-example.R
+++ code/bhet-med-analysis-example.R
@@ -3,7 +3,7 @@
 #  input:    bhet-master; indoor temp files
 #  output:   
 #  project:  BHET
-#  author:   sam harper \ 2023-12-21
+#  author:   sam harper \ 2023-12-16
 
 
 ##  0 Load needed packages ----
@@ -31,89 +31,386 @@ library(labelled)
 options(mc.cores = 4,
         brms.backend = "cmdstanr")
 
-## download data files from OSF (data-clean component)
-# dir.create("data-clean")
-# bhet_project <- osf_retrieve_node("b4wze")
-# bhet_project %>%
-#   osf_ls_files("Master Dataset (Season 4)",
-#                pattern = "dta") %>%
-#   osf_download(path = here("data-clean"),
-#     conflicts = "overwrite")
+# read in data
+d3 <- readRDS(here("data-clean", "d3"))
+
+te_med <-
+  brm(data = d3, 
+      family = bernoulli(),
+      resp ~ 1 + (1 | v_id) + 
+        treat:cohort_year_2019:year_2019 + 
+        treat:cohort_year_2019:year_2021 +
+        treat:cohort_year_2020:year_2021 +
+        treat:cohort_year_2021:year_2021 +
+        cohort_year_2019 + cohort_year_2020 +
+        cohort_year_2021 + year_2019 + year_2021,
+      prior = c(prior(normal(0, 1), class = Intercept),
+       prior(normal(0, 1), class = b),
+       prior(exponential(1), class = sd)),
+      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      sample_prior = "yes", 
+      seed = 3857,
+      file = "code/fits/bhet-med-te_med")
+
+# load if model already exists
+te_med <- readRDS("code/fits/bhet-resp-te_med.rds")
+
+## check the chains
+mcmc_trace(te_med, pars=c("b_Intercept", "b_year_2021")) +
+  theme_classic() + ylab("")
+
+## extract prior draws
+pd <- prior_draws(te_med) %>%
+  # rescale to absolute probabilities
+  mutate(pd0 = inv_logit_scaled(Intercept),
+         pd1 = inv_logit_scaled(Intercept + b)) %>%
+  # treatment effect
+  mutate(diff = pd1 - pd0)
+
+## plot for intercept prior
+plot_pd1 <- pd %>%
+  ggplot(aes(x = pd1)) + 
+  stat_halfeye(color= "black", fill =  '#1b9e77') +
+  labs(x = "Probability of poor lung health", 
+    y = NULL, subtitle = "Prior for Intercept") +
+  theme_classic()
+
+## plot for treatment effect prior
+plot_pdd <- pd %>%
+  ggplot(aes(x = diff)) + 
+  stat_halfeye(color= "black", fill =  '#d95f02') +
+  labs(x = "Difference in treated vs. control", 
+    y = NULL, subtitle = "Prior for treatment effect") +
+  theme_classic()
+
+# Combined plot for priors
+priors_lung <- (plot_pd1 | plot_pdd) +
+  plot_annotation(
+    title = "Priors for poor lung symptoms",
+                  theme = theme_classic())
+
+# Marginal predictions for Bayesian ETWFE (simple)
+
+## load brms model
+# te_med <- readRDS(here("code/fits", 
+ # "bhet-med-te_med.rds"))
+
+bme_pred <- predictions(
+  te_med, 
+  newdata   = subset(d3, treat==1),
+  variables = "treat", 
+  by        = "treat"
+  )
+
+# plot of predicted probabilities by treatment
+bme_pred_p <- bme_pred |>
+  posterior_draws() |>
+  ggplot(aes(x = draw, fill=factor(treat))) +
+    stat_halfeye(slab_alpha = .5) + 
+    annotate("text", x = 0.61, y = 0.7, 
+           label="Control", color='#1b9e77') +
+    annotate("text", x = 0.49, y = 0.95, 
+           label="Treated", color='#d95f02') +
+  scale_x_continuous(
+    "Probability of poor respiratory symptoms", 
+    limits=c(0.4,0.8)) +
+  scale_y_continuous("Posterior Density") +
+  scale_fill_manual(values = c('#1b9e77','#d95f02')) +
+  theme_classic() + 
+  theme(legend.position = "none", axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(size=12))
+
+bme_avg <- slopes(
+  te_med, 
+  newdata   = subset(d3, treat==1),
+  variables = "treat", 
+  by        = "treat"
+  ) 
+
+# plot of treatment effect
+bme_avg_p <- bme_avg |>
+  posterior_draws() |>
+  ggplot(aes(x = draw)) +
+    stat_halfeye(slab_alpha = .5, fill = "#7570b3") +
+    annotate("text", x = -0.106, y = 0.95, 
+           label="Difference", color = '#7570b3') +
+    geom_vline(xintercept = 0, linetype = "dashed",
+               color = "gray60") +
+    scale_x_continuous("Marginal Effect", limits=c(-0.3,0.1)) +
+    scale_y_continuous("") +
+    theme_classic() + 
+    theme(legend.position = "none", 
+        axis.text.y = element_blank(),
+        axis.line.y = element_blank(), 
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(size=12))
 
 
-## 1 Read in dataset, limit to resp vars ----
-d <- read_dta(here("data-clean", 
-                   "BHET_master_data_15Dec2023.dta"), 
-  col_select= c(hh_id, ptc_id, wave, ID_VILLAGE, 
-                ban_status_2019, ban_status_2020, 
-                ban_status_2021, ban_status_no, 
-                ban_status_composite, smoking,
-                freq_cough, freq_phlegm,
-                freq_wheezing, freq_breath,
-                freq_no_chest, health_selfreport,
-                freq_exercising, hours_sleep)) 
+f2 <- bme_pred_p + bme_avg_p + 
+  plot_layout(widths = c(1, 1)) +
+  plot_annotation(title = "Posterior distributions of marginal predictions: poor respiratory symptoms")
+f2
 
-# define main outcome (as binary)
-d1 <- d %>% drop_na() %>%
-  mutate(resp = if_else(
-    freq_cough < 3 |
-    freq_phlegm < 3 |
-    freq_wheezing < 3 |
-    freq_breath < 3 |
-    freq_no_chest < 3, 1, 0),
-    fphealth = if_else(health_selfreport > 2, 1, 0, 
-                       missing = NA),
-    srh = health_selfreport,
-    fex = freq_exercising,
-    hsleep = hours_sleep,
-    year = if_else(wave==1, 2018, 
-      if_else(wave==2, 2019,
-        if_else(wave==4, 2021, 0))),
-    cohort_year = if_else(
-      ban_status_composite==1, 2019, 
-      if_else(ban_status_composite==2, 2020, 
-              if_else(ban_status_composite==3, 2021, 2022))),
-    treat = ifelse(year >= cohort_year, 1, 0),
-    cohort_year = ifelse(cohort_year == 2022,-Inf, 
-                         cohort_year)) %>%
-  # relabel last cohort year 
-  # treatment cohort dummies
-  add_dummy_variables(cohort_year, 
-    values=c(-Inf,2019,2020,2021), 
-    remove_original = F) %>%
-  # wave dummies
-  add_dummy_variables(year, 
-    values=c(2018,2019,2021), remove_original = F)
 
-dt <- readxl::read_excel(here("data-clean", 
-  "HH_indoor_temperature_HEATING_SEASON.xlsx")) %>%
-  select(ptc_id, wave, min_h, med_h, max_h,
-         sd_h, iqr_h)
+# wrangle aggregate ATTs for model summary table
+bmp <- data.frame(
+  term = bme_pred$treat,
+  estimate = bme_pred$estimate,
+  conf.low = bme_pred$conf.low,
+  conf.high = bme_pred$conf.high,
+  std.error = abs(bme_pred$conf.high - 
+                    bme_pred$conf.low) / (2 * 1.96)
+)
 
-# merge indoor temp data with resp data
-d2 <- d1 %>%
-  left_join(dt, by = join_by(ptc_id,wave))
+bti <- data.frame(
+  term = 2,
+  estimate = bme_avg$estimate,
+  conf.low = bme_avg$conf.low,
+  conf.high = bme_avg$conf.high,
+  std.error = abs(bme_avg$conf.high - 
+                    bme_avg$conf.low) / (2 * 1.96)
+)
 
-# limit sample
+bta <- bind_rows(bmp,bti) %>%
+  mutate(term = recode_factor(term,
+    `0` = "Untreated", `1` = "Treated",
+    `2` = "Difference"))
 
-d3 <- d2 %>% 
-  select(starts_with(c("year","cohort")), 
-  "ID_VILLAGE","resp","treat", "med_h",
-  "min_h", "max_h", "sd_h", "iqr_h", 
-  "smoking", "srh", "fphealth", "fex",
-  "hsleep") %>%
-  
-  # limit to complete cases
-  drop_na() %>% 
-  
-  # create unique continuous village Id
-  group_by(ID_VILLAGE) %>%
-  mutate(v_id = cur_group_id()) %>%
-  ungroup()
+gl <- data.frame()
 
-# remove labels
-val_labels(d3) <- NULL
+betwfe_me_avg <- list(tidy = bta, glance = gl)
+class(betwfe_me_avg) <- "modelsummary_list"
 
-# write to data-clean
-saveRDS(d3, file = here("data-clean", "d3"))
+modelsummary(list("Total Effect" = betwfe_me_avg),
+  shape = term ~ model + statistic, 
+  statistic = c("({std.error})", "conf.int"),
+  gof_omit ='._*')
 
+
+# by cohort
+bme_c <- slopes(
+  te_med, 
+  newdata   = subset(d3, treat & cohort_year),
+  variables = "treat", 
+  by        = "cohort_year"
+  )
+
+bti_c <- data.frame(
+  term = paste("ATT(g", bme_c$cohort_year, ")", sep=""), 
+  estimate = bme_c$estimate,
+  conf.low = bme_c$conf.low,
+  conf.high = bme_c$conf.high,
+  std.error = abs(bme_c$conf.high - 
+                    bme_c$conf.low) / (2 * 1.96)
+)
+
+betwfe_me_c <- list(tidy = bti_c, glance = gl)
+class(betwfe_me_c) <- "modelsummary_list"
+
+modelsummary(list("Bayesian Cohort Average" = betwfe_me_c),
+  shape = term ~ model + statistic, 
+  statistic = c("({std.error})", "conf.int"),
+  gof_omit ='._*')
+
+
+# mediation model with interaction
+cde_med <-
+  brm(data = d3, 
+      family = bernoulli(),
+      resp ~ 1 + (1 | v_id) + hsleep +
+        treat:cohort_year_2019:year_2019 + 
+        treat:cohort_year_2019:year_2021 +
+        treat:cohort_year_2020:year_2021 +
+        treat:cohort_year_2021:year_2021 +
+        treat:cohort_year_2019:year_2019:hsleep + 
+        treat:cohort_year_2019:year_2021:hsleep +
+        treat:cohort_year_2020:year_2021:hsleep +
+        treat:cohort_year_2021:year_2021:hsleep +
+        cohort_year_2019 + cohort_year_2020 +
+        cohort_year_2021 + year_2019 + year_2021,
+      prior = c(prior(normal(0, 1), class = Intercept),
+        prior(normal(0, 1), class = b),
+        prior(exponential(1), class = sd)),
+      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      sample_prior = "yes", 
+      seed = 2326,
+      file = "code/fits/bhet-med-cde_med")
+
+# marginal effects
+bcde_pred <- predictions(
+  cde_med, 
+  newdata   = subset(d3, treat==1),
+  variables = "treat", 
+  by        = "treat"
+  )
+
+bcde_avg <- slopes(
+  cde_med, 
+  newdata   = subset(d3, treat==1),
+  variables = "treat", 
+  by        = "treat"
+  ) 
+
+# wrangle aggregate ATTs for model summary table
+bmpc <- data.frame(
+  term = bcde_pred$treat,
+  estimate = bcde_pred$estimate,
+  conf.low = bcde_pred$conf.low,
+  conf.high = bcde_pred$conf.high,
+  std.error = abs(bcde_pred$conf.high - 
+                    bcde_pred$conf.low) / (2 * 1.96)
+)
+
+btic <- data.frame(
+  term = 2,
+  estimate = bcde_avg$estimate,
+  conf.low = bcde_avg$conf.low,
+  conf.high = bcde_avg$conf.high,
+  std.error = abs(bcde_avg$conf.high - 
+                    bcde_avg$conf.low) / (2 * 1.96)
+)
+
+btca <- bind_rows(bmpc,btic) %>%
+  mutate(term = recode_factor(term,
+    `0` = "Untreated", `1` = "Treated",
+    `2` = "Difference"))
+
+gl <- data.frame()
+
+bcde_me_avg <- list(tidy = btca, glance = gl)
+class(bcde_me_avg) <- "modelsummary_list"
+
+modelsummary(list("CDE" = bcde_me_avg),
+  shape = term ~ model + statistic, 
+  statistic = c("({std.error})", "conf.int"),
+  gof_omit ='._*')
+
+# both estimates
+modelsummary(list("Total Effect" = betwfe_me_avg,
+  "CDE" = bcde_me_avg),
+  shape = term ~ model + statistic, 
+  statistic = "conf.int",
+  gof_omit ='._*')
+
+# mediation model with interaction
+cde_med_temp <-
+  brm(data = d3, 
+      family = bernoulli(),
+      resp ~ 1 + (1 | v_id) + min_h +
+        treat:cohort_year_2019:year_2019 + 
+        treat:cohort_year_2019:year_2021 +
+        treat:cohort_year_2020:year_2021 +
+        treat:cohort_year_2021:year_2021 +
+        treat:cohort_year_2019:year_2019:min_h + 
+        treat:cohort_year_2019:year_2021:min_h +
+        treat:cohort_year_2020:year_2021:min_h +
+        treat:cohort_year_2021:year_2021:min_h +
+        cohort_year_2019 + cohort_year_2020 +
+        cohort_year_2021 + year_2019 + year_2021,
+      prior = c(prior(normal(0, 1), class = Intercept),
+        prior(normal(0, 1), class = b),
+        prior(exponential(1), class = sd)),
+      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      sample_prior = "yes", 
+      seed = 2326,
+      file = "code/fits/bhet-med-cde_med_temp")
+
+
+
+# does the policy affect self-reported health?
+te_hsleep <-
+  brm(data = d3, 
+      family = gaussian(),
+      hsleep ~ 1 + (1 | v_id) +
+        treat:cohort_year_2019:year_2019 + 
+        treat:cohort_year_2019:year_2021 +
+        treat:cohort_year_2020:year_2021 +
+        treat:cohort_year_2021:year_2021 +
+        cohort_year_2019 + cohort_year_2020 +
+        cohort_year_2021 + year_2019 + year_2021,
+      prior = c(prior(normal(0, 10), class = Intercept),
+        prior(normal(0, 5), class = b),
+        prior(exponential(1), class = sd)),
+      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      sample_prior = "yes", 
+      seed = 265,
+      file = "code/fits/te_hsleep")
+
+# load if already run
+te_temp_min <- readRDS("code/fits/te_temp_min.rds")
+
+ndc <- subset(d3, treat==1) %>%
+  mutate(mean_h=20)
+
+
+# mediator model
+m_model <- bf(hsleep ~ 1 + (1 | v_id) +
+                treat:cohort_year_2019:year_2019 + 
+                treat:cohort_year_2019:year_2021 +
+                treat:cohort_year_2020:year_2021 +
+                treat:cohort_year_2021:year_2021 +
+                cohort_year_2019 + cohort_year_2020 +
+                cohort_year_2021 + year_2019 + year_2021) +
+                gaussian()
+
+# outcome model
+y_model <- bf(resp ~ 1 + (1 | v_id) + min_h + 
+                treat:cohort_year_2019:year_2019 + 
+                treat:cohort_year_2019:year_2021 +
+                treat:cohort_year_2020:year_2021 +
+                treat:cohort_year_2021:year_2021 +
+                cohort_year_2019 + cohort_year_2020 +
+                cohort_year_2021 + year_2019 + year_2021) +
+                bernoulli()
+
+# priors
+priormed <- c(
+  prior(normal(10, 5), class = Intercept, resp = minh),
+  prior(normal(0, 5), class = b, resp = minh),
+  prior(exponential(1), class = sd, resp = minh),
+  prior(normal(0, 1), class = Intercept, resp = resp),
+  prior(normal(0, 1), class = b, resp = resp),
+  prior(exponential(1), class = sd, resp = resp))
+
+medfit <- brm(
+  m_model + y_model + set_rescor(FALSE),
+  data = d3, iter = 2000, warmup = 1000,
+  prior = priormed,
+  chains = 4, cores = 4,
+  sample_prior = "yes")
+
+
+brm(data = d3, 
+      family = bernoulli(),
+      resp ~ 1 + (1 | v_id) + min_h +
+        treat:cohort_year_2019:year_2019 + 
+        treat:cohort_year_2019:year_2021 +
+        treat:cohort_year_2020:year_2021 +
+        treat:cohort_year_2021:year_2021 +
+        cohort_year_2019 + cohort_year_2020 +
+        cohort_year_2021 + year_2019 + year_2021,
+      prior = c(prior(normal(0, 1), class = Intercept),
+                prior(normal(0, 1), class = b),
+                prior(exponential(1), class = sd)),
+      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      sample_prior = "yes", 
+      seed = 265,
+      file = "code/fits/bhet-resp-cde_med_min_ni")
+
+
+ndm <- tibble(treat = 0:1, hsleep = 7.74)
+
+draws <- as_draws_df(cde_med)
+
+# add predicted estimates
+pd <- ndm %>% 
+  add_epred_draws(cde_med, allow_new_levels=TRUE)
+
+# summarize
+pd |> 
+  filter(.category == "y") |> 
+  pivot_wider(id_cols = .draw, 
+    names_from = x, values_from = .epred) |> 
+  mutate(diff = `1` - `0`) |> 
+  median_qi(diff)
